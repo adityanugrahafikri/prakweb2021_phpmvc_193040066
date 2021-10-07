f987011b4067db76b508c30e3ea12093a34bad6e
adityanugrahafikri <91908458+adityanugrahafikri@users.noreply.github.com>
2021-10-08 02:41:34 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman <?= $data['judul']; ?></tittle>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+</head>
+<body>
+
+<!-- As a link -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">PHP MVC</a>
+  </div>
+</nav>
+
+<!-- As a heading -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Navbar</span>
+  </div>
+</nav>
